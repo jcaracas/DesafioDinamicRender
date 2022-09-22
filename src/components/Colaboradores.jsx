5f3c5be0c59f8c5bd7f939b243a4e8c0b3c9b46b
JoseC <josecaracas@gmail.com>
2022-09-21 23:07:44 -0300

    commit css

--- src/components/Colaboradores.jsx
+++ src/components/Colaboradores.jsx
@@ -12,9 +12,9 @@ function Colaboradores(props) {
                     <ListGroup horizontal key={props.id}>
                         <ListGroup.Item className="col2">{props.name}</ListGroup.Item>
                         <ListGroup.Item className="col2">{props.email}</ListGroup.Item>
-                        <ListGroup.Item className="col1 p-1">
-                            <Button className="mb-0" variant="outline-success" type="submit" >Editar</Button>
-                            <Button className="mb-0" variant="outline-danger" type="submit" >Eliminar</Button>
+                        <ListGroup.Item className="col1">
+                            <Button className="m-0" variant="outline-secondary" type="submit" >Editar</Button>
+                            <Button className="m-0" variant="outline-danger" type="submit" >Eliminar</Button>
                         </ListGroup.Item>
                     </ListGroup>   
                 </ListGroup>  
